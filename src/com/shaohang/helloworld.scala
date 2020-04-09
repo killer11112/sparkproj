328e7a9ea1d2275355146a3bbd646b474357ba0d
killer11112 <1093084958@qq.com>
2020-04-09 16:15:55 +0800

    Initial commit local

---
+++ src/com/shaohang/helloworld.scala
@@ -0,0 +1,21 @@
+package com.shaohang
+import org.apache.spark.{SparkContext, SparkConf}
+
+
+object helloworld {
+  def main(args : Array[String]) = {
+    val conf = new SparkConf().setAppName("WordCount")
+    val sc = new SparkContext(conf)
+    val input = args(0)
+    val texts = sc.textFile("a.txt").map(line => line.split(" "))
+      .flatMap(words => words.map(word => (word.replaceAll("[^A-Za-z]", ""), 1)))
+    val counts =  texts.reduceByKey(_ + _)
+    counts.collect.foreach{
+      case (word, num) =>
+        println(word + " " + num.toString)
+    }
+
+  }
+
+
+}
